228dccb101c83159bf79f53468fb009a5627def9
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-02-05 19:32:31 +0530

    fix: add bytesToString(), helper function for converting Uint8Array to string

--- src/index.ts
+++ src/index.ts
@@ -8,28 +8,32 @@ import bencode from "bencode";
 // data (like info hashes) that would be corrupted if forced into a string.
 const data = bencode.decode(fs.readFileSync("puppy.torrent"));
 
+// helper function to convert Uint8Array (Buffer) to string
+const bytesToString = (b: Uint8Array): string =>
+  Buffer.from(b).toString("utf-8");
+
 // listing files in the torrent: multi-file vs single-file
 if (data.info.files) {
   data.info.files.forEach((file: { path: Buffer[]; length: number }) => {
     // path is an array of Buffers, map each buffer to a string
     const pathString = file.path
-      .map((p: Buffer) => p.toString("utf-8"))
+      .map((p: Uint8Array) => bytesToString(p))
       .join("/");
     console.log(`File: ${pathString} (${file.length} bytes)`);
   });
 } else {
   // name is a Buffer
   console.log(
-    `File: ${data.info.name.toString("utf-8")} (${data.info.length} bytes)`,
+    `File: ${bytesToString(data.info.name)} (${data.info.length} bytes)`,
   );
 }
 
 // metadata fields of torrent file:
-console.log("Announce URL:", data.announce.toString("utf-8"));
+console.log(`Announce URL: ${bytesToString(data.announce)}`);
 console.log(
   "Comment:",
   data.comment ? data.comment.toString("utf-8") : "No comment",
 );
-console.log("Name:", data.info.name.toString("utf-8"));
+console.log(`Name: ${bytesToString(data.info.name)}`);
 console.log("Piece Length:", data.info["piece length"]);
 console.log("Pieces:", data.info.pieces.length / 20, "pieces");
