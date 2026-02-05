40442961a41bce8236787a140e705438e19d3885
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-02-05 19:12:18 +0530

    feat: outputs metadata of torrent file to console
    this take in account a torrent file with single and multiple files,
    and outputs the metadata of each file in the torrent.

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,28 @@ import bencode from "bencode";
 // data (like info hashes) that would be corrupted if forced into a string.
 const data = bencode.decode(fs.readFileSync("puppy.torrent"));
 
-// manually convert only the fields we know are text
-console.log(data.announce.toString("utf-8"));
+// listing files in the torrent: multi-file vs single-file
+if (data.info.files) {
+  data.info.files.forEach((file: { path: Buffer[]; length: number }) => {
+    // path is an array of Buffers, map each buffer to a string
+    const pathString = file.path
+      .map((p: Buffer) => p.toString("utf-8"))
+      .join("/");
+    console.log(`File: ${pathString} (${file.length} bytes)`);
+  });
+} else {
+  // name is a Buffer
+  console.log(
+    `File: ${data.info.name.toString("utf-8")} (${data.info.length} bytes)`,
+  );
+}
+
+// metadata fields of torrent file:
+console.log("Announce URL:", data.announce.toString("utf-8"));
+console.log(
+  "Comment:",
+  data.comment ? data.comment.toString("utf-8") : "No comment",
+);
+console.log("Name:", data.info.name.toString("utf-8"));
+console.log("Piece Length:", data.info["piece length"]);
+console.log("Pieces:", data.info.pieces.length / 20, "pieces");
