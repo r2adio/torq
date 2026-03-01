e9539a0277e68b037da9a7e1dcdae08a95757512
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-03-01 08:47:37 +0530

    chore(ui): design the layout of footer

--- app/ui/app.ts
+++ app/ui/app.ts
@@ -1,16 +1,51 @@
 import { ASCIIFont, Box, Text, TextAttributes } from "@opentui/core";
+function footer() {
+  return Box(
+    {
+      alignItems: "stretch",
+      justifyContent: "space-between",
+      position: "absolute",
+      paddingX: 3,
+      bottom: 2,
+      height: 0,
+      width: "100%",
+      // borderStyle: "single",
+      flexDirection: "row",
+    },
+    // project name, version
+    Box(
+      { flexDirection: "row", alignItems: "center", gap: 1 },
+      Text({ content: "torq", attributes: TextAttributes.DIM }),
+      Text({ content: `v0.1.0`, attributes: TextAttributes.DIM }),
+    ),
+    // project navigation hints
+    Box(
+      { flexDirection: "row", alignItems: "center", gap: 1 },
+      Text({ content: "torq", attributes: TextAttributes.DIM }),
+      Text({ content: "v0.1.0", attributes: TextAttributes.DIM }),
+    ),
+    // meta info about client, eg: port, upload/download speed, connected peers, etc.
+    Box(
+      { flexDirection: "row", alignItems: "center", gap: 1 },
+      Text({ content: `Port: `, attributes: TextAttributes.DIM }),
+      Text({ content: "v0.1.0", attributes: TextAttributes.DIM }),
+    ),
+  );
+}
+
 export function createApp() {
   return Box(
     {
       alignItems: "center",
       justifyContent: "center",
       flexGrow: 1,
-      borderStyle: "single",
+      // borderStyle: "single",
     },
     Box(
       { justifyContent: "center", alignItems: "flex-end" },
       ASCIIFont({ font: "tiny", text: "torq" }),
       Text({ content: "a BitTorrent client", attributes: TextAttributes.DIM }),
     ),
+    footer(),
   );
 }
