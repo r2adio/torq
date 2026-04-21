5e664b43ff23e46bc3f3dab9150de2d83aa09ad1
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-04-21 08:14:30 +0530

    feat: update tui from @opentui/core -> @opentui/solid

---
+++ app/ui/index.tsx
@@ -0,0 +1,38 @@
+import { render } from "@opentui/solid";
+const App = () => <text>Hello, World!</text>;
+render(App);
+// import { createCliRenderer } from "@opentui/core";
+// import { Box } from "@opentui/core";
+// import {
+//   extras,
+//   footer,
+//   network,
+//   stats,
+//   torrents,
+//   meta,
+// } from "@/ui/components";
+//
+// interface Renderer {
+//   isDestroyed: boolean;
+//   requestLive(): void;
+//   dropLive(): void;
+//   on(event: "destroy", cb: () => void): void;
+// }
+//
+// export function createApp(renderer: Renderer) {
+//   return Box(
+//     { flexDirection: "column", paddingBottom: 1 },
+//     Box({ flexDirection: "row" }, torrents(renderer), meta(renderer)),
+//     Box(
+//       { flexDirection: "row", justifyContent: "flex-start" },
+//       network(renderer),
+//       extras(renderer),
+//       stats(renderer),
+//     ),
+//     Box({}, footer(renderer)),
+//   );
+// }
+//
+// const renderer = await createCliRenderer({ exitOnCtrlC: true, targetFps: 30 });
+// renderer.root.add(createApp(renderer));
+// renderer.start();
